322c2957ee145be85dfec820c2af9c0d5290d85f
Ayla-van-de-vosse <ayla@vandevosse.net>
2025-09-09 14:57:45 +0200

    Update Main.java
    Gaming Library

--- src/nl/isy_games/Main.java
+++ src/nl/isy_games/Main.java
@@ -9,6 +9,19 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Welkom bij Gaming Library!");
+        System.out.println("Kies spel:");
+        System.out.println("1. TicTacToe");
+        System.out.println("2. Nog niet beschikbaar");
+        int keuzeSpel = scanner.nextInt();
+
+        BoardGame game;
+        if (keuzeSpel == 1) {
+            game = new TicTacToeGame();
+        } else {
+//            game = new gaming.games.LaterSpel();
+        }
+
         System.out.println("Welkom bij TicTacToe!");
         System.out.println("Kies modus:");
         System.out.println("1. Mens vs Computer");
