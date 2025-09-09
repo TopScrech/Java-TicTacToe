6efe298a54d8b2d55f681f454febec5934e16207
Ayla-van-de-vosse <ayla@vandevosse.net>
2025-09-09 13:17:19 +0200

    Update Main.java
    Begin keuze

--- src/nl/isy_games/Main.java
+++ src/nl/isy_games/Main.java
@@ -6,7 +6,12 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welkom bij TicTacToe!");
+        System.out.println("Kies modus:");
+        System.out.println("1. Mens vs Computer");
+        System.out.println("2. Computer vs Computer");
 
         AI ai = new AI();
         System.out.print(ai.name);
