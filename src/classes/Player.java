bcc6fe1c65418f87ae4a7de62b3482481b3f8b82
TopScrech <sergei_saliukov@icloud.com>
2025-09-09 13:01:20 +0200

    added a comment about initializing a Player

--- src/classes/Player.java
+++ src/classes/Player.java
@@ -4,7 +4,10 @@ public class Player {
     String name;
 
     // protected makes the constructor accessible within the same package and by subclasses
+    // Init example:
+    // Player p1 = new Player("Wijmar");
     protected Player(String name) {
         this.name = name;
     }
+
 }
--- src/classes/TicTacToeGame.java
+++ src/classes/TicTacToeGame.java
@@ -1,5 +1,5 @@
 package classes;
 
 public class TicTacToeGame extends BoardGame {
-    Player p1 = new Player("");
+
 }
