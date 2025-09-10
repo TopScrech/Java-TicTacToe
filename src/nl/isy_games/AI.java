507189a1828f311b6b55fa1f0002fe7630721a17
Ayla-van-de-vosse <ayla@vandevosse.net>
2025-09-10 14:53:53 +0200

    Update AI.java
    AI kiest een willekeurige set

--- src/nl/isy_games/AI.java
+++ src/nl/isy_games/AI.java
@@ -1,7 +1,28 @@
 package nl.isy_games;
 
 public class AI extends Player {
+    private Random random;
     public AI() {
         super("Wijmar-Ultimate-3000");
+        random = new Random();
+    }
+
+    public int[] chooseMove(BoardGame game) {
+        int row, col;
+        int[] move = new int[2];
+        boolean valid = false;
+
+        while (!valid) {
+            row = random.nextInt(3);
+            col = random.nextInt(3);
+
+            // check lege cel via TicTacToeGame
+            if (((TicTacToeGame) game).isCellEmpty(row, col)) {
+                move[0] = row;
+                move[1] = col;
+                valid = true;
+            }
+        }
+        return move;
     }
 }
