d9c18baf78543cce83a07eb365f57db6c53bf535
TopScrech <sergei_saliukov@icloud.com>
2025-09-11 10:38:37 +0200

    updated the lang dictionary and improved code readability

--- src/nl/isy_games/Main.java
+++ src/nl/isy_games/Main.java
@@ -1,21 +1,19 @@
 package nl.isy_games;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welkom bij Gaming Library!");
         System.out.println("Kies spel:");
         System.out.println("1. TicTacToe");
         System.out.println("2. Nog niet beschikbaar");
+
+        Scanner scanner = new Scanner(System.in);
         int keuzeSpel = scanner.nextInt();
 
         BoardGame game;
+
         if (keuzeSpel == 1) {
             game = new TicTacToeGame();
         } else {
@@ -27,6 +25,7 @@ public class Main {
         System.out.println("Kies modus:");
         System.out.println("1. Mens vs Computer");
         System.out.println("2. Computer vs Computer");
+
         int keuze;
 
         if (scanner.hasNextInt()) {
@@ -48,6 +47,7 @@ public class Main {
         }
 
         Player current = player1;
+
         while (!game.isGameOver()) {
             game.printBoard();
             System.out.println("Beurt: " + current.getName());
@@ -62,19 +62,24 @@ public class Main {
                 } else {
                     System.out.println("Voer rij (0-2) in:");
                     int row = scanner.nextInt();
+
                     System.out.println("Voer kolom (0-2) in:");
                     int col = scanner.nextInt();
+
                     moveDone = game.makeMove(current, row, col);
+
                     if (!moveDone) {
                         System.out.println("Ongeldige zet, probeer opnieuw!");
                     }
                 }
             }
+
             current = (current == player1) ? player2 : player1;
         }
 
         game.printBoard();
         Player winner = game.getWinner();
+
         if (winner != null) {
             System.out.println("Winnaar: " + winner);
         } else {
